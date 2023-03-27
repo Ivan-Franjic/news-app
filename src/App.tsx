95ff9bb62a7460d7a40ec5692c4072817471f870
Franke266 <ifranjic099@gmail.com>
2023-03-27 16:17:14 +0200

    design changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import News from "./Pages/News";
-import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
+import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,7 +2,9 @@ import "./Header.scss";
 export default function Header() {
   return (
     <div className="header">
-      <h1 className="header__title">News</h1>
+      <span className="header__title">
+        <span className="header__title--red">My</span>News
+      </span>
       <form>
         <input
           className="header__search"

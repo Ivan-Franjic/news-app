fc488d0a5ca5b86fa327f1035be9022715cf98da
Franke266 <ifranjic099@gmail.com>
2023-03-28 13:20:45 +0200

    design changes

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -7,59 +7,67 @@ import { MdOutlineScience } from "react-icons/md";
 import { IoFootball } from "react-icons/io5";
 import { FiMonitor } from "react-icons/fi";
 import { AiFillStar } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <Link className="sidebar__link" to={`/`}>
-        <div className="sidebar__item">
-          <HiHome className="sidebar__icon" />
-          <span className="sidebar__text">Home</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/general`}>
-        <div className="sidebar__item">
-          <IoNewspaperOutline className="sidebar__icon" />
-          <span className="sidebar__text">General</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/business`}>
-        <div className="sidebar__item">
-          <ImBriefcase className="sidebar__icon" />
-          <span className="sidebar__text">Business</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/health`}>
-        <div className="sidebar__item">
-          <BiHealth className="sidebar__icon" />
-          <span className="sidebar__text">Health</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/science`}>
-        <div className="sidebar__item">
-          <MdOutlineScience className="sidebar__icon" />
-          <span className="sidebar__text">Science</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/sports`}>
-        <div className="sidebar__item">
-          <IoFootball className="sidebar__icon" />
-          <span className="sidebar__text">Sports</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/technology`}>
-        <div className="sidebar__item">
-          <FiMonitor className="sidebar__icon" />
-          <span className="sidebar__text">Technology</span>
-        </div>
-      </Link>
-      <Link className="sidebar__link" to={`/favourites`}>
-        <div className="sidebar__item">
-          <AiFillStar className="sidebar__icon" />
-          <span className="sidebar__text">Favourites</span>
-        </div>
-      </Link>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/home`}
+      >
+        <HiHome className="sidebar__icon" />
+        <span className="sidebar__text">Home</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/entertainment`}
+      >
+        <IoNewspaperOutline className="sidebar__icon" />
+        <span className="sidebar__text">Entertainment</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/business`}
+      >
+        <ImBriefcase className="sidebar__icon" />
+        <span className="sidebar__text">Business</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/health`}
+      >
+        <BiHealth className="sidebar__icon" />
+        <span className="sidebar__text">Health</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/science`}
+      >
+        <MdOutlineScience className="sidebar__icon" />
+        <span className="sidebar__text">Science</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/sports`}
+      >
+        <IoFootball className="sidebar__icon" />
+        <span className="sidebar__text">Sports</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/technology`}
+      >
+        <FiMonitor className="sidebar__icon" />
+        <span className="sidebar__text">Technology</span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={`/favourites`}
+      >
+        <AiFillStar className="sidebar__icon" />
+        <span className="sidebar__text">Favourites</span>
+      </NavLink>
     </div>
   );
 }

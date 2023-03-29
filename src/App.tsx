f2d23ea9d6b1fe755f5822daa4f713ff719a80fa
Franke266 <ifranjic099@gmail.com>
2023-03-29 15:29:54 +0200

    components removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import News from "./Pages/News";
-import Header from "./Components/Header/Header";
-import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Sidebar />
         <Routes>
           <Route
             path="/"

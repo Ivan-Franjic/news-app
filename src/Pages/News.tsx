dd51fb98233ecdb432ea39ebc1511fb28b6b9a1f
Franke266 <ifranjic099@gmail.com>
2023-03-31 11:26:54 +0200

    sidebar toggler added

--- src/Pages/News.tsx
+++ src/Pages/News.tsx
@@ -6,12 +6,23 @@ import useSWR from "swr";
 import { useParams } from "react-router-dom";
 import Header from "../Components/Header/Header";
 import Sidebar from "../Components/Sidebar/Sidebar";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 export default function News() {
   const { categoryName } = useParams()!;
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState<string>("");
   const [category, setCategory] = useState<string>("news");
   const [search, setSearch] = useState<string>("");
+  const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
+  const [ToggleState, setToggleState] = useState<number>(1);
+
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
+
+  const getActiveClass = (index: number, className: string) =>
+    ToggleState === index ? className : "";
 
   const fetchData = () => {
     if (categoryName === undefined) {
@@ -38,45 +49,76 @@ export default function News() {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div className="news">
-      <div className="news__header">
+    <div className="homepage">
+      <div className="homepage__header">
         <Header
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setSearch(e.target.value);
           }}
           onClick={fetchData}
         />
-        <div className="header__buttons">
-          <p>Featured</p>
-          <p>Latest</p>
-        </div>
-        <div className="border"></div>
+        <button
+          className="sidebar__toggler"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        >
+          {navbarOpen ? (
+            <MdClose style={{ width: "32px", height: "32px" }} />
+          ) : (
+            <FiMenu
+              style={{
+                width: "32px",
+                height: "32px",
+              }}
+            />
+          )}
+        </button>
+        <ul className="header__buttons">
+          <li
+            className={`tab ${getActiveClass(1, "active-tab")}`}
+            onClick={() => toggleTab(1)}
+          >
+            Featured
+          </li>
+          <li
+            className={`tab ${getActiveClass(2, "active-tab")}`}
+            onClick={() => toggleTab(2)}
+          >
+            Latest
+          </li>
+        </ul>
+        <div className="header__border"></div>
       </div>
-      <div className="news__main">
-        <div className="news__sidebar">
+      <div className="homepage__main">
+        <div className={`homepage__sidebar${navbarOpen ? " show-menu" : ""}`}>
           <Sidebar />
         </div>
-        <div className="news__content">
-          <div className="news__articles">
+        <div className="homepage__content">
+          <div className="content__articles">
             <p>News</p>
-            <div className="articles">
-              {data.articles.map((item: any) => {
-                return (
-                  <Article
-                    title={item.title}
-                    category={category}
-                    author={item.author}
-                    image={item.urlToImage}
-                  />
-                );
-              })}
+            <div className={`content ${getActiveClass(1, "active-content")}`}>
+              <div className="articles">
+                {data.articles.map((item: any) => {
+                  return (
+                    <Article
+                      title={item.title}
+                      category={category}
+                      author={item.author}
+                      image={item.urlToImage}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
-          <div className="news__latest">
-            <LatestNews />
+          <div className={`content ${getActiveClass(2, "active-content")}`}>
+            <div className="content__latest">
+              <LatestNews />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
+}
+{
 }

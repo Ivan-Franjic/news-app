8a6997925d21e3f65fd37481119167abf252f70b
Franke266 <ifranjic099@gmail.com>
2023-04-03 17:01:56 +0200

    search fixed

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,16 +3,18 @@ import "./Header.scss";
 export default function Header({
   onClick,
   onChange,
+  onKeyPress,
 }: {
   onClick: React.MouseEventHandler;
   onChange: React.ChangeEventHandler;
+  onKeyPress: React.KeyboardEventHandler;
 }) {
   return (
     <div className="header">
       <span className="header__title">
         <span className="header__title--red">My</span>News
       </span>
-      <div className="header__search">
+      <div className="header__search" onKeyPress={onKeyPress}>
         <input type="search" placeholder="Search news" onChange={onChange} />
         <button onClick={onClick} className="header__button" type="submit">
           SEARCH

ceb65652cfa8f69e2c4ed744317d33462a216144
Franke266 <ifranjic099@gmail.com>
2023-04-25 17:46:14 +0200

    infinite scroll added

--- src/Components/LatestNews/LatestNews.tsx
+++ src/Components/LatestNews/LatestNews.tsx
@@ -1,13 +1,38 @@
 import "./LatestNews.scss";
-import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
+const perPage = 20;
+
 export default function LatestNews() {
+  function handleScroll(e: any) {
+    const bottom =
+      Math.abs(
+        e.target.scrollHeight - e.target.clientHeight - e.target.scrollTop
+      ) < 1;
+    if (bottom) {
+      setSize(size + 1);
+    }
+  }
+
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR(
-    `https://newsapi.org/v2/top-headlines?q=&country=us&apiKey=ff6ab548f10341ddb1c486d6399fdbc2`,
-    fetcher
+
+  const { data, error, size, setSize } = useSWRInfinite(
+    (index) =>
+      `https://newsapi.org/v2/top-headlines?country=us&pageSize=${perPage}&page=${
+        index + 1
+      }&apiKey=ff6ab548f10341ddb1c486d6399fdbc2`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateAll: false,
+      revalidateFirstPage: false,
+    }
   );
   if (error) return <div>Request Failed</div>;
   if (!data) return <div>Loading...</div>;
+
+  const news = data ? [].concat(...data) : [];
 
   return (
     <div className="latest-news">
@@ -15,17 +40,19 @@ export default function LatestNews() {
         <span className="header__dot"></span>
         <span>Latest news</span>
       </div>
-      <div className="latest-news__articles">
-        {data.articles.map((item: any, index: number) => {
-          return (
-            <div key={index}>
-              <p className="latest-news__time">
-                {item.publishedAt.substr(11, 5)}
-              </p>
-              <p className="latest-news__title">{item.title}</p>
-              <p className="article__border"></p>
-            </div>
-          );
+      <div className="latest-news__articles" onScroll={handleScroll}>
+        {news.map((item: any) => {
+          return item.articles.map((article: any, index: number) => {
+            return (
+              <div key={index}>
+                <p className="latest-news__time">
+                  {article.publishedAt.substr(11, 5)}
+                </p>
+                <p className="latest-news__title">{article.title}</p>
+                <p className="article__border"></p>
+              </div>
+            );
+          });
         })}
       </div>
       <div className="latest-news__footer">
